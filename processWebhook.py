0d60650c23ee231dc96d06a2315dc465854191a1
kremeth <66464637+kremeth@users.noreply.github.com>
2022-07-23 00:58:39 +0400

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -52,6 +52,7 @@ def get_closest(name):
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument('--window-size=1920,1080')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
